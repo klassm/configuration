107c51ed13c6a3ceb9c71b88f7eeb8d01259bdc0
Matthias Klass <matthias.klass@gmail.com>
2018-03-19 19:55:16 +0100

    initial

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ PWD="$(pwd)"
 # Link nixos configuration
 NIXOS_SRC="${PWD}/nixos"
 NIXOS_DEST="/etc/nixos"
-sudo ln -fs "${NIXOS_SRC}/config" "${NIXOS_DEST}/config"
-sudo ln -fs "${NIXOS_SRC}/configuration.nix" "${NIXOS_DEST}/configuration.nix"
 
+sudo rm -f "${NIXOS_DEST}/config" "${NIXOS_DEST}/configuration.nix"
+sudo ln -s "${NIXOS_SRC}/config" "${NIXOS_DEST}/config"
+sudo ln -fs "${NIXOS_SRC}/configuration.nix" "${NIXOS_DEST}/configuration.nix"
 
 stow -d dotfiles -t ~  X zsh vim screenlayout i3 git
 
